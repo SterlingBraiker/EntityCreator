1757624c3fafb147070a85371f0e981060056ee2
T M <goombaba@gmail.com>
2024-06-25 18:21:21 -0400

    Testing Load image with File, convert to Bytes, load image from &[u8] Testing with PngImage isn't working yet

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use rusqlite::*;
-
+use std::io::{Write, Read, Seek, SeekFrom};
+use std::fs::File;
+use fltk::{prelude::*, window::Window, app, image, frame::Frame};
 
 #[derive(Default)]
 struct RecordSet {
@@ -36,8 +38,63 @@ enum SqlData {
 }
 
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), rusqlite::Error> {
+
+    let fltk_app: app::App = app::App::default();
+    let mut wind: fltk::window::DoubleWindow = Window::new(100, 100, 400, 300, "My window");
+    let mut png_img: image::PngImage = image::PngImage::load("C:\\Users\\goomb\\Downloads\\Screenshot 2024-06-15 141905.png").unwrap();
+    let mut png_img_file = File::open("C:\\Users\\goomb\\Downloads\\Screenshot 2024-06-15 141905.png").unwrap();
+    let mut frame = Frame::default().with_size(360, 260).center_of(&wind);
+
+    let bytes: Vec<u8> = png_img_file.bytes().map(|x| x.unwrap()).collect();
+
+
+    let db = Connection::open_in_memory()?;
+
+    db.execute_batch("CREATE TABLE test_table (content BLOB);")?;
+
+    db.execute("INSERT INTO test_table (content) VALUES (ZEROBLOB(242434))", [])?;
+
+    let row_id = db.last_insert_rowid();
+
+    let mut blob = db.blob_open(DatabaseName::Main, "test_table", "content", row_id, false)?;
+
+    //let bytes_written = blob.write(b"01234567").unwrap_or(0);
+
+    let bytes_written = blob.write(&png_img.to_rgb_data()[..]).unwrap_or(0);
+    println!("bytes written: {}", bytes_written);
+
+    blob.seek(SeekFrom::Start(0)).unwrap_or(0);
+
+    let mut buf = [0u8; 242434];
+
+    let bytes_read = blob.read(&mut buf[..]).unwrap_or(0);
+    println!("bytes read: {}", bytes_read);
+
+
+    println!("png img about to load");
+    let mut new_png_img = fltk::image::PngImage::from_data(&bytes[..]);
+    println!("png img loaded");
+
+    match new_png_img {
+        Ok(mut img) => {
+            frame.draw(move |f| { 
+                println!("frame drawing");
+                img.scale(f.w(), f.h(), true, true);
+                println!("frame scaled");
+                img.draw(f.x() + 40, f.y(), f.w(), f.h());
+                println!("frame drewed");
+            });
+        },
+        Err(_) => {},
+    }
+        
+    wind.end();
+    wind.show();
+
+    fltk_app.run().unwrap();
+
+    Ok(())
 }
 
 
@@ -47,11 +104,6 @@ fn query(
     params: &[(&str, &dyn ToSql)],
 ) -> RecordSet {
     
-    // build out an empty recordset
-/*    let rs: RecordSet = create_empty_rs();
-    let mut headers: Array<GString> = rs.gd_recordset.get("headers").as_ref().unwrap().to();
-    let mut gd_rs: Array<Array<Variant>> = rs.gd_recordset.get("records").as_ref().unwrap().to();
- */
     let mut rs: RecordSet = RecordSet::default();
 
     let stmt: Result<CachedStatement, Error> = sqlite_connection.prepare_cached(query_str);
@@ -87,7 +139,6 @@ fn query(
                             // get each field in this row and put it in the gd_recordset
                             // converting each SQLite value to a Godot equivalent
                             match &e[ind] {
-
                                 types::Value::Null =>                     { new_row.fields.push( SqlData::Null ) },
                                 types::Value::Integer(v_i64) =>     { new_row.fields.push( SqlData::Integer(v_i64.clone()) ) },
                                 types::Value::Real(v_f64) =>        { new_row.fields.push( SqlData::Real(v_f64.clone()) ) },
@@ -113,3 +164,4 @@ fn query(
     rs
 
 }
+
